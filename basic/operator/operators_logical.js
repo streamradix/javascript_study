339eb9c9909032adff325c390d02351eb43b65df
streamradix <111672276+streamradix@users.noreply.github.com>
2022-12-08 19:06:09 +0900

    Operator: Logical &&, ||, !
    Logical AND: &&
    Logical OR: ||
    Logical NOT: !

---
+++ basic/operator/operators_logical.js
@@ -0,0 +1,19 @@
+const LINE_BREAK = "";
+
+console.log("===== Logical AND : &&");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(LINE_BREAK);
+console.log("===== Logical OR : ||");
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(LINE_BREAK);
+console.log("===== Logical NOT : !");
+console.log(!true);
+console.log(!false);
